7140f5f5aacc1eed9ad137af6e9dcc7af0bbebc6
Matheus <matheuzinhobpupim205@gmail.com>
2024-07-04 17:38:31 -0300

    Exemplo de listas em Python

---
+++ Aula 13/iteracao_while.py
@@ -0,0 +1,26 @@
+lista1 = [1, 2, 11, 66, 71, 24, 72, 1, 4]
+lista2 = ['P', 'y', 't', 'h', 'o', 'n']
+lista3 = [1, 2, 'a', 'b']
+lista4 = []
+lista5 = list(range(10, -1, -1))
+lista6 = list("Curso de Python")
+
+# exemplo de iteração com while
+listaJogos = []
+produtos = ''
+
+while produtos != '0':
+    produtos = input("Informe o nome de um jogo para adicioná-lo na lista de jogos ou digite 0 para sair:\n")
+    if produtos != '0':
+        listaJogos.append(produtos)
+    else:
+        print("Finalizando")
+
+opcao = input("Deseja exibir a lista de jogos? Digite 's' para aceitar ou 'n' para negar: ").lower()
+if opcao == 's':
+    contagem = 1
+    for jogos in listaJogos:
+        print(f"Jogo número {contagem}:", jogos)
+        contagem += 1
+else:
+    exit()
---
+++ Aula 13/iterando_listas.py
@@ -0,0 +1,12 @@
+lista1 = [1, 2, 11, 66, 71, 24, 72, 1, 4]
+lista2 = ['P', 'y', 't', 'h', 'o', 'n']
+lista3 = [1, 2, 'a', 'b']
+lista4 = []
+lista5 = list(range(10, -1, -1))
+lista6 = list("Curso de Python")
+
+soma = ''
+for elemento in lista6:
+    print(elemento)
+    soma += elemento
+    print("A soma de todos os valores da lista é:", soma)
