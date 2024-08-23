042daeaa7dbcf8fd5bb524e5d0c418dc6fe628b3
Matheus <matheusdevcj@gmail.com>
2024-08-23 17:40:09 -0300

    Commits da aula 22 do curso de Python

---
+++ Aula 22/funcoes_default_parameters.py
@@ -0,0 +1,27 @@
+print('Curso de Python')
+print()
+
+
+def multiplica_numero(numero1, numero2=9715):
+    return numero1 * numero2 
+
+
+print(multiplica_numero(5, 3))
+print(multiplica_numero(12.1))
+
+
+def exemplo_melhor(nome='João', aluno=False):
+    if nome == 'João' and aluno:
+
+        return f'Bem-vindo {nome}'
+    elif not aluno:
+
+        return f'Olá {nome}, você não é um aluno.'
+    return f'Olá {nome}'
+
+
+print(exemplo_melhor)()
+print((exemplo_melhor(aluno=True)))
+print(exemplo_melhor(False))
+print((exemplo_melhor(aluno=True)))
+print(exemplo_melhor(nome='Maria'))
