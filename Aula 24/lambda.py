df02b52b23191140f6515a7650656d8875ff3a0a
Matheus <matheusdevcj@gmail.com>
2024-08-28 18:45:46 -0300

    Atividade lambda e Projeto de uma Calculadora em Python

---
+++ Aula 24/lambda.py
@@ -0,0 +1,14 @@
+def funcao(x, y):
+    return x * y
+
+
+print(funcao(4, 7))
+
+lambda x, y: 4 * 7
+nomes = ['Isaac Bradbury', 'Robert Asimov', 'Arthur Herbert', 'Frank Heinlein', 'Leigh Adams',\
+        'Clark H. Brackket', 'Orson Scott'] 
+        
+print(nomes)
+
+nomes.sort(key=lambda sobrenome: sobrenome.split(' ')[-1].lower())
+print(nomes)
---
+++ Projeto Calculadora/projeto_calculadora.py
@@ -0,0 +1,72 @@
+from tkinter import *
+
+# Definindo configurações da janela
+janela = Tk()
+janela.title("Calculadora Python")
+janela.geometry("400x405")
+janela.config(bg="#1f1f1e")
+
+
+# Criação dos frames do visor
+frame_tela = Frame(janela, width=400, height=100, bg="#38576b")
+frame_tela.grid(row=0, column=0)
+
+
+# Criação dos frames do corpo da calculadora
+frame_corpo = Frame(janela, width=400, height=305)
+frame_corpo.grid(row=1, column=0)
+
+
+# Criação dos botões
+# Linha 1
+btnC = Button(frame_corpo, text="C", width=15, height=2, bg="#ECEFF1",
+              font='Consolas 15 bold', relief=RAISED, overrelief=RIDGE)
+btnC.place(x=0, y=0)
+
+btnPorcentagem = Button(frame_corpo, text="%", width=10, height=2, bg="#ECEFF1",
+                        font='Consolas 15 bold', relief=RAISED, overrelief=RIDGE)
+btnPorcentagem.place(x=165, y=0)
+
+btnDivisao = Button(frame_corpo, text="/", width=10, height=2, bg="#FFAB40",
+                    font='Consolas 15 bold', relief=RAISED, overrelief=RIDGE)
+btnDivisao.place(x=280, y=0)
+
+
+# Linha 2
+btn7 = Button(frame_corpo, text="7", width=8, height=2, bg="#ECEFF1",
+              font='Consolas 15 bold', relief=RAISED, overrelief=RIDGE)
+btn7.place(x=-2, y=61)
+
+btn8 = Button(frame_corpo, text="8", width=8, height=2, bg="#ECEFF1",
+              font='Consolas 15 bold', relief=RAISED, overrelief=RIDGE)
+btn8.place(x=94, y=61)
+
+btn9 = Button(frame_corpo, text="9", width=8, height=2, bg="#ECEFF1",
+              font='Consolas 15 bold', relief=RAISED, overrelief=RIDGE)
+btn9.place(x=190, y=61)
+
+btnMultiplica = Button(frame_corpo, text="*", width=10, height=2, bg="#FFAB40",
+                       font='Consolas 15 bold', relief=RAISED, overrelief=RIDGE)
+btnMultiplica.place(x=280, y=61)
+
+
+# Linha 3
+btn4 = Button(frame_corpo, text="4", width=8, height=2, bg="#ECEFF1",
+              font='Consolas 15 bold', relief=RAISED, overrelief=RIDGE)
+btn4.place(x=-2, y=122)
+
+btn5 = Button(frame_corpo, text="5", width=8, height=2, bg="#ECEFF1",
+              font='Consolas 15 bold', relief=RAISED, overrelief=RIDGE)
+btn5.place(x=94, y=122)
+
+btn6 = Button(frame_corpo, text="6", width=8, height=2, bg="#ECEFF1",
+              font='Consolas 15 bold', relief=RAISED, overrelief=RIDGE)
+btn6.place(x=190, y=122)
+
+btnSubtrai = Button(frame_corpo, text="-", width=10, height=2, bg="#FFAB40",
+                    font='Consolas 15 bold', relief=RAISED, overrelief=RIDGE)
+btnSubtrai.place(x=280, y=122)
+
+
+# Execução
+janela.mainloop()
